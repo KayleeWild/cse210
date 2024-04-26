122eff5bb0142c40de8ff60fe4a21d6bfa5a441c
KayleeWild <crazykaylee7@byui.edu>
2024-04-26 12:42:46 -0600

    Completed Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Ask for first name
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+        //Ask for last name
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+        //Display James Bond dialog
+        Console.WriteLine($"\nYour name is {last_name}, {first_name} {last_name}.");
     }
 }
