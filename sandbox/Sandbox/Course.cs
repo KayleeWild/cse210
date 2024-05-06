f0d764a851913bc3d046b5bbd25d1c90fa91e8f1
KayleeWild <crazykaylee7@byui.edu>
2024-05-06 12:08:03 -0600

    added a little course thing

---
+++ sandbox/Sandbox/Course.cs
@@ -0,0 +1,14 @@
+class Course
+{
+    public string _courseCode;
+    public string _className;
+    public int _numberOfCredits;
+    public string _color;
+
+    // method
+    public void Display()
+    {
+        
+        Console.Write($"{_courseCode} {_className} {_numberOfCredits} {_color}");
+    }
+}
