ada487fd1998831f6cdfe24c0e2fd86da01be6bd
KayleeWild <crazykaylee7@byui.edu>
2024-05-10 23:53:52 -0600

    job/resume prep assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job
+        {
+            _company = "Microsoft",
+            _jobTitle = "Software Engineer",
+            _startYear = 2019,
+            _endYear = 2022
+        };
+        job1.DisplayJob();
+        Job job2 = new Job
+        {
+            _company = "Apple",
+            _jobTitle = "Manager",
+            _startYear = 2022,
+            _endYear = 2023
+        };
+        job2.DisplayJob();
+        Resume myResume = new();
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume._name = "Allison Rose";
+        myResume.DisplayResume();
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,15 @@
+class Resume
+{
+    public string _name;
+    public List<Job> _jobs = [];
+
+    public void DisplayResume()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+        foreach (Job job in _jobs)
+        {
+            job.DisplayJob();
+        }
+    }
+}
